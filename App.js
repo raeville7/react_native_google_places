5b37786770e8c9ff1126db9767e78f4362b046c8
raeville7 <raeville7@gmail.com>
2020-05-31 01:25:20 +0800

    added material ui and logic

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Appbar, RadioButton, Checkbox } from 'react-native-paper';
 
 export default class App extends React.Component {
   constructor(props){
@@ -19,41 +20,90 @@ export default class App extends React.Component {
       searchType: ['']
     }
   }
+  _goBack = () => console.log('Went back');
+  // set strict bounds yes or no
+  handleStrictBounds = () => {
+    var isChecked = this.state.isStrictBounds;
+    this.setState({
+      isStrictBounds: !isChecked,
+      checked: !isChecked
+    }); 
+    
+    if(!isChecked){
+      this.getCountryShortName();
+    }
+    else {
+      this.setState({ country: null });
+    } 
+  }
+  getCountryShortName = () => {
+    var addr = this.state.addrComp;            
+    for (var i=0; i<addr.length; i++) {
+      if (addr[i].types[0] === "country") {
+        var cnty = addr[i].short_name; 
+        this.setState({
+          country: 'country:' + cnty
+        });
+      }
+    }
+  }
+  handleChangeOption = (val) => {
+    this.setState({ 
+      option: val,
+      searchType: val === 'all' ? [''] : val
+    });
+  }
   render() {
     return (
       <>
-        <GooglePlacesAutocomplete
-          placeholder='Enter Location'
-          debounce={200}
-          style={{flex: 1}}
-          minLength={2}
-          autoFocus={true}
-          fetchDetails={true}
-          onPress={(data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            this.setState({ 
-              location: details.geometry.location,
-              addrComp: details.address_components 
-            })
-          }}
-          query={{
-            key: 'AIzaSyAQTIQGNx6fdYSsaSqlqK4AEO89LdkIslk',
-            language: 'en',
-            type: this.state.searchType,
-            components: this.state.country
-          }}
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction
+          onPress={this._goBack}
         />
-        <View style={styles.mapContainer}>
-          <MapView
+        <Appbar.Content
+          title="Google Places Autocomplete"
+        />
+      </Appbar.Header>
+      <View style={styles.options}>
+          <RadioButton.Group
+            onValueChange={value => this.handleChangeOption(value)}
+            value={this.state.option}    
+          >
+            <View style={styles.eachOption}>
+              <RadioButton value="all" />
+              <Text style={styles.textSpacing}>All</Text>  
+            </View>
+            <View style={styles.eachOption}>
+              <RadioButton value="establishment" />
+              <Text style={styles.textSpacing}>Establishment</Text>             
+            </View>
+            <View style={styles.eachOption}>
+              <RadioButton value="address" />
+              <Text style={styles.textSpacing}>Address</Text>
+            </View>
+            <View style={styles.eachOption}>
+              <RadioButton value="geocode" />
+              <Text style={styles.textSpacing}>Geocode</Text>
+            </View>          
+          </RadioButton.Group>
+        </View>
+        <View style={styles.bounds}>
+          <Checkbox
+            status={this.state.checked ? 'checked' : 'unchecked'}
+            onPress={() => this.handleStrictBounds()}
+          />
+          <Text style={styles.textSpacing}>Strict Bounds</Text>
+        </View>
+        <View style={styles.main}>   
+          <MapView style={StyleSheet.absoluteFillObject}
             provider={PROVIDER_GOOGLE}
-            style={{flex: 1}}
             region={{
               latitude: this.state.location.lat,
               longitude: this.state.location.lng,
               latitudeDelta: 0.069,
               longitudeDelta: 0.049,
             }}    
-          >
+          >           
             <Marker 
               coordinate={{ 
                 latitude: this.state.location.lat, 
@@ -61,8 +111,31 @@ export default class App extends React.Component {
               }}
               draggable />
           </MapView>
-        </View>
         
+          <View style={{ position: 'absolute', top: 10, left: 0, right: 0  }}>
+            <GooglePlacesAutocomplete
+              placeholder='Enter Location'
+              debounce={200}
+              style={{backgroundColor: 'white', color: 'black'}}
+              minLength={2}
+              autoFocus={true}
+              fetchDetails={true}
+              onPress={(data, details = null) => {
+                // 'details' is provided when fetchDetails = true
+                this.setState({ 
+                  location: details.geometry.location,
+                  addrComp: details.address_components 
+                })
+              }}
+              query={{
+                key: 'AIzaSyAQTIQGNx6fdYSsaSqlqK4AEO89LdkIslk',
+                language: 'en',
+                type: this.state.searchType,
+                components: this.state.country
+              }}
+            />
+          </View>
+        </View>
       </>  
     );
   }
@@ -75,7 +148,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  mapContainer: {
-    height: '70%'
-  }
+  header: {
+    backgroundColor: '#ADCFFF'
+  },
+  main: {
+    justifyContent: 'flex-end',
+    height: '80%'
+  },
+  options:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  eachOption: {
+    flexDirection: 'row'
+  },
+  textSpacing: {
+    paddingTop: 8
+  },
+  bounds:{
+    alignSelf: 'center',
+    flexDirection: 'row'
+  },
 });
